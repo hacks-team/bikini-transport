66e63b4f6372de91f3cb9b35704011fb60fb134a
jangseyeong <jang.se.yeong000@gmail.com>
2025-11-21 13:03:01 +0900

    add CouponButton Layout

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,6 +1,7 @@
 import { ark } from '@ark-ui/react';
 import { css } from 'styled-system/css';
 import { Box, Flex } from 'styled-system/jsx';
+import { CouponButton } from '@/components/CouponButton';
 import { Header } from '@/layout/Header';
 import { Button } from '@/ui-lib/components/Button';
 import { openRouteOptionsBottomSheet } from './components/RouteOptionsBottomSheet';
@@ -16,9 +17,15 @@ export const HomePage = () => {
         </Header.Left>
       </Header>
 
-      <Box flex="1" overflowY="auto" px={5} pb={5} pt="45%" zIndex={1}>
-        <RouteSearchSection />
-      </Box>
+      <Flex direction="column" flex="1" overflowY="auto" pb="6" zIndex={1}>
+        <Box flex="1" px={5} pb={5} pt="45%">
+          <RouteSearchSection />
+        </Box>
+
+        <Box px="2.5" hidden={false}>
+          <CouponButton />
+        </Box>
+      </Flex>
 
       <Box px={5} py={4} flexShrink={0} backgroundColor="background.normal" zIndex={1}>
         <Button fullWidth onClick={openRouteOptionsBottomSheet}>
